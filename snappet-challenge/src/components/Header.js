97eff87b61976fdeba8abfb7a54a4bb724973a4a
nikolasalim <nikolasalim@gmail.com>
2020-06-16 14:12:56 +0200

    Add Link placeholders for navigation

--- snappet-challenge/src/components/Header.js
+++ snappet-challenge/src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+  // Leaving Link placeholders for navigation links:
   return (
     <div>
-      <div className="navigation">Dashboard / Snappet /</div>
-      <div className="title">Front-End Challenge</div>
+      <div className="top-box">
+        <div className="navigation">
+          <Link to="/">Dashboard</Link> / <Link to="/">Snappet</Link> /
+        </div>
+        <div className="title">Front-End Challenge</div>
+      </div>
+      <div className="bottom-box"></div>
     </div>
   );
 }
