e68d121db79c5bd47f8d04bed680022c3cbbb728
nikolasalim <nikolasalim@gmail.com>
2020-06-16 14:08:27 +0200

    Set up react-router-dom

--- snappet-challenge/src/App.js
+++ snappet-challenge/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Route path="/" component={Header} />
+      <Switch></Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ snappet-challenge/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div className="navigation">Dashboard / Snappet /</div>
+      <div className="title">Front-End Challenge</div>
+    </div>
+  );
+}
+
+export default Header;
