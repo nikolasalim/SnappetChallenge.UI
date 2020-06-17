e82722b13ddf01cfd41cee990f89b87e9a7e8bc1
nikolasalim <nikolasalim@gmail.com>
2020-06-17 12:53:03 +0200

    Style header

--- snappet-challenge/src/components/Header/index.js
+++ snappet-challenge/src/components/Header/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Header.scss";
+import { ReactComponent as GiftIcon } from "../../assets/icons/gift.svg";
+
 function Header() {
   // Leaving Link placeholders for navigation links:
   return (
-    <div>
+    <div className="body">
       <div className="top-box">
         <div className="navigation">
-          <Link to="/">Dashboard</Link> / <Link to="/">Snappet</Link> /
+          <Link to="/" className="link">
+            Dashboard
+          </Link>{" "}
+          /{" "}
+          <Link to="/" className="link">
+            Snappet
+          </Link>{" "}
+          /
         </div>
-        <div className="title">Front-End Challenge</div>
+        <div className="title">
+          <GiftIcon className="gift-icon" />
+          Front-End Challenge
+        </div>
       </div>
       <div className="bottom-box"></div>
     </div>
